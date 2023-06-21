4cbac13dbcbffe8f20f0d70874d9798654c34480
Gavaskar <gavaskaraeg@gmail.com>
2023-06-21 22:30:49 +0530

    menu card added

---
+++ ipod/src/commponent/display.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Display = ()=>{
+    return(
+       <div className='container ipodjs'>
+        <h3 className='title-ipod'>iPod.js</h3>
+        <ul className='menu-list' type="none">
+        <div>
+            <li>coverFlow</li>
+            </div>
+            <div>
+            <li>music</li>
+            </div>
+            <div>
+            <li>Game</li>
+            </div>
+            <div>
+            <li>setting</li>
+            </div>
+           
+           
+        </ul>
+        
+ 
+
+       </div>
+    );
+}
+
+export default Display;
--- ipod/src/commponent/phoneCover.jsx
+++ ipod/src/commponent/phoneCover.jsx
@@ -1,35 +1,59 @@
 import React from 'react'
-
-const Phone = ()=>{
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForwardFast } from '@fortawesome/free-solid-svg-icons';
+import { faBackwardFast } from '@fortawesome/free-solid-svg-icons';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
+import Display from './display';
+class Phone extends React.Component{
+   constructor(){
+    super();
+    this.state={
+      showMenu:false
+    }
+   }
+   menuCard = ()=>{
+    console.log('menu is pressed',this.state.showMenu);
+    this.setState({
+      showMenu:true
+    })
+   }
+  render(){
+    const {showMenu} = this.state;
     return(
       <div id="outer" className='container ' style={{height:"100%"}}>
           <div className='container phone'>
 
             <div className='  display'>
-            
-
+            {showMenu && <Display/>}
+             
             </div>
             <div className="container  buttons">
   <div className="d-flex flex-column align-items-center justify-content-center rounded-circle button-center">
-    <div className="button">MENU</div>
+  <div className="button" onClick={this.menuCard}  style={{ fontFamily:'verdana' }}>
+  MENU
+</div>
   </div>
   <div className="d-flex flex-row justify-content-between">
     <div className="d-flex flex-column align-items-center justify-content-center rounded-circle button-left">
-      <div>back</div>
+      <div><FontAwesomeIcon icon={faBackwardFast} style={{ color: '#061e47' }}/></div>
     </div>
     <div className='rounded-circle okbutton border border-1'></div>
     <div className="d-flex flex-column align-items-center justify-content-center rounded-circle button-right">
-      <div>forward</div>
+      <div>      <FontAwesomeIcon icon={faForwardFast} style={{ color: '#061e47' }} />
+</div>
     </div>
   </div>
   <div className="d-flex flex-column align-items-center justify-content-center rounded-circle button-bottom">
-    <div>pause</div>
+    <div><FontAwesomeIcon icon={faPlay} /> <FontAwesomeIcon icon={faPause} /></div>
   </div>
 </div>
 
           </div>
       </div>
     )
+}
+
 }
 
 export default Phone;
